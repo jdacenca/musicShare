410d6178cb5764202a5c1d62a2b12a6c490f9bc3
HaripriyaGounalan <haripriyaviveck@gmail.com>
2024-11-07 19:47:50 -0500

    Added Spotify card
    Style changes

--- src/components/MusicFeed.jsx
+++ src/components/MusicFeed.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import MusicPost from "./MusicPost";
 import user1 from "../assets/images/user1.jpg";
 import user2 from "../assets/images/user2.jpg";
-import album1 from "../assets/images/album1.jpg";
+import user3 from "../assets/images/user3.jpg";
 import album2 from "../assets/images/album2.jpg";
 
 function MusicFeed() {
@@ -15,22 +15,35 @@ function MusicFeed() {
         id: 1,
         username: "dj_breezy",
         userImage: user1,
-        musicImage: album1,
         description: "Check out this awesome track!",
         likes: 10,
         comments: [
           { username: "user1", text: "Love this!" },
           { username: "user2", text: "Amazing track!" },
         ],
+        videoUrl: "https://www.youtube.com/embed/CevxZvSJLk8", // Video link
       },
       {
         id: 2,
         username: "melody_lover",
         userImage: user2,
-        musicImage: album2,
         description: "This song hits different!",
         likes: 5,
-        comments: [{ username: "user3", text: "Can’t stop listening!" }],
+        comments: [{ username: "user3", text: "Can’t stop listening!" }
+        ],
+        musicImage: album2,
+      },
+      {
+        id: 3,
+        username: "vinyl_vibes",
+        userImage: user3,
+        description: "Throwback vibes with memories!",
+        likes: 15,
+        comments: [
+          { username: "user4", text: "Brings back memories!" },
+          { username: "user5", text: "An all-time favorite!" },
+        ],
+        spotifyUrl: "3nR9B40hYLKLcR0Eph3Goc", // Spotify track ID for Memories
       },
     ]);
   }, []);
--- src/components/MusicPost.jsx
+++ src/components/MusicPost.jsx
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
-import Comment from './Comment';
+import React, { useState } from "react";
+import Comment from "./Comment";
 import "../styles/MusicPost.css";
-import { Heart, MessageCircle, Share2 } from 'react-feather';
+import { Heart, MessageCircle, Share2 } from "react-feather";
 
 function MusicPost({ post }) {
   const [likes, setLikes] = useState(post.likes);
   const [comments, setComments] = useState(post.comments);
-  const [commentText, setCommentText] = useState('');
-  const [showComments, setShowComments] = useState(true);
+  const [commentText, setCommentText] = useState("");
+  const [showComments, setShowComments] = useState(false);
 
   const handleLike = () => {
     setLikes(likes + 1);
@@ -16,8 +16,11 @@ function MusicPost({ post }) {
   const handleCommentSubmit = (e) => {
     e.preventDefault();
     if (commentText) {
-      setComments([...comments, { username: 'current_user', text: commentText }]);
-      setCommentText('');
+      setComments([
+        ...comments,
+        { username: "current_user", text: commentText },
+      ]);
+      setCommentText("");
     }
   };
 
@@ -28,22 +31,50 @@ function MusicPost({ post }) {
   return (
     <div className="music-post card shadow-xss rounded-xxl border-0 p-4 mb-3">
       <div className="post-header">
-        <img src={post.userImage || "default-user.png"} alt="User" className="user-avatar" />
+        <img
+          src={post.userImage || "default-user.png"}
+          alt="User"
+          className="user-avatar"
+        />
         <h3>{post.username || "Anonymous"}</h3>
       </div>
       <div className="post-body">
-        <img src={post.musicImage} alt="Music Cover" className="music-cover" />
+        {post.videoUrl ? (
+          <iframe
+            width="100%"
+            height="500"
+            src={post.videoUrl}
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        ) : post.spotifyUrl ? (
+          <iframe
+            src={`https://open.spotify.com/embed/album/${post.spotifyUrl}`}
+            width="100%"
+            height="360px"
+            allow="encrypted-media"
+            allowFullScreen
+            title="Spotify player"
+          ></iframe>
+        ) : (
+          <img
+            src={post.musicImage}
+            alt="Music Cover"
+            className="music-cover"
+          />
+        )}
         <p>{post.description}</p>
       </div>
       <div className="post-footer">
-        <button onClick={handleLike} className='like-button'>
+        <button onClick={handleLike} className="like-button">
           <Heart /> Like ({likes})
         </button>
-        <button onClick={toggleComments} className='comment-button'>
+        <button onClick={toggleComments} className="comment-button">
           <MessageCircle /> Comments
         </button>
-        <button className='share-button'>
-          <Share2/> Share
+        <button className="share-button">
+          <Share2 /> Share
         </button>
       </div>
       {showComments && (
