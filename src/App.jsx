4eaee8e5e2883e7b2d88c9d38e6ce2ab1c4d42ad
HaripriyaGounalan <haripriyaviveck@gmail.com>
2024-11-20 19:34:39 -0500

    navbar update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import Navbar from "./components/NavBar";
 import MusicFeed from "./components/MusicFeed";
 import Header from "./components/Header";
 import SideBar from "./components/SideBar";
+import NavBar from "./components/Navbar";
 import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 
@@ -21,9 +21,9 @@ function App() {
       <Header isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode} />
 
         <div className="row g-0"> {/* Removes Bootstrap grid gap */}
-          {/* Navbar (Left Sidebar) */}
+          {/* NavBar (Left Sidebar) */}
           <div className="d-none d-md-block p-4 col-md-3 navbar-container">
-            <Navbar isDarkMode={isDarkMode}/>
+            <NavBar isDarkMode={isDarkMode}/>
           </div>
 
           {/* Music Feed (Center Content) */}
