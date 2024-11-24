534ba345081196a1b602d4af754d628d8ab88f1d
Jeanne Damasco <jdamasc1@my.centennialcollege.ca>
2024-11-24 11:42:20 -0500

    Added functionality to search in youtube music

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,11 @@
 import dotenv from "dotenv";
 import express from "express";
-import axios from "axios";
+
 import { spotifyAuth } from "./utils/spotifyAuth.js";
 import { spotifyGlobalTopHits } from "./utils/spotifyGlobalTopHits.js";
 import { spotifyGenreRecommendation } from "./utils/spotifyGenreRecommendation.js";
+import { youtubeMusic } from "./utils/youTubeSearchMusicCategory.js";
+
 import cors from "cors";
 
 dotenv.config({ path: "../.env" });
@@ -20,6 +22,8 @@ const port = process.env.PORT || 3000;
 app.post("/spotify/connect", spotifyAuth);
 app.get("/spotify/trending", spotifyGlobalTopHits);
 app.get("/spotify/recommendations", spotifyGenreRecommendation);
+
+app.get("/youtube/music/search", youtubeMusic);
 
 app.listen(port, () => {
   console.log("Server is running on port: " + port);
---
+++ server/src/utils/youTubeSearchMusicCategory.js
@@ -0,0 +1,31 @@
+'use strict';
+
+import axios from 'axios';
+
+export const youtubeMusic = async function(req, res) {
+    const { q } = req.query;
+    let youtubeKey = process.env.YOUTUBE_API_KEY;
+    let youtubeBaseURL = process.env.YOUTUBE_BASE_API_URL;
+    let url = youtubeBaseURL + '/search?key=' + youtubeKey;
+    if (q != undefined) {
+        url = url + "&q=" + encodeURIComponent(q);
+    }
+    url = url + '&part=snippet&type=video&videoCategoryId=10&channelType=any&eventType=none&videoEmbeddable=true&maxResults=10'    
+
+    console.log(url)
+
+    axios.get(url)
+    .then((response) => {
+        if (response.status === 200) {
+            return res.status(200).send(response.data);
+        } else {
+            return res.status(401).send("Access Denied");
+        }
+        
+    })
+    .catch((error) => {
+        console.log("Error getting the authorization token. " +  error);
+        return res.status(500).send("Internal Server Error");
+    }) 
+    
+};
