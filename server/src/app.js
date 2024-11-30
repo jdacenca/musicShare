9326043e89777e4987df4758819770a6b76b36d0
Jeanne Damasco <jdamasc1@my.centennialcollege.ca>
2024-11-30 11:21:57 -0500

    Added the endpoint

--- server/src/app.js
+++ server/src/app.js
@@ -5,7 +5,7 @@ import { spotifyAuth, spotifyAuthFunc } from "./utils/spotifyAuth.js";
 import { spotifyGlobalTopHits } from "./utils/spotifyGlobalTopHits.js";
 import { spotifyGenreRecommendation } from "./utils/spotifyGenreRecommendation.js";
 import { youtubeMusic } from "./utils/youTubeSearchMusicCategory.js";
-import { databaseConnect, databaseDisconnect } from "./utils/databaseHelper.js";
+import { databaseConnect, databaseDisconnect, getPost, insertPost, updatePostMessage, updatePostLike, deletePostMessage } from "./utils/databaseHelper.js";
 import { sendEmail } from "./utils/sendEmail.js";
 import { router, databasePoolConnect, authenticateToken } from "./utils/authRoutes.js";
 import { startSocketIOServer } from './utils/notification.js'
@@ -34,6 +34,13 @@ app.post("/spotify/connect", spotifyAuth);
 app.get("/spotify/trending", spotifyGlobalTopHits);
 app.get("/spotify/recommendations", spotifyGenreRecommendation);
 app.get("/youtube/music/search", youtubeMusic);
+app.post("/post/create", insertPost); // {userId:'ACC0000002', message:'Look at this song! <3', url:'https://open.spotify.com/track/3yfqSUWxFvZELEM4PmlwIR'} 
+app.post("/posts", getPost); // {userId:'ACC0000002', sort:'ASC'}
+app.post("/post/update", updatePostMessage); // {postId:'PST000003', message:'Soooooooooo addicted to this song!'}
+app.post("/post/like/update", updatePostLike); // {postId:'PST000003', noOfLikes:123}
+app.delete("/post", deletePostMessage); // {userId:'ACC0000002'}
+
+
 
 //app.use("/api/", router)
 app.use("/auth", router); // auth routes
